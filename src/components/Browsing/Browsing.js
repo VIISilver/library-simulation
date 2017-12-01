36a79a419e6eb15843b89dfbe42568b80e51acec
VIISilver <jparke71@hotmail.com>
2017-12-01 14:03:37 -0700

    Auth0 is almost setup. Basic routing setup. Block elements implemented.

---
+++ src/components/Browsing/Browsing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Browsing extends Component {
+  render() {
+    return (
+      <div >
+          
+      </div>
+    );
+  }
+}
+
+export default Browsing;
